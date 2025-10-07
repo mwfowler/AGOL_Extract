36279c1301c5c90eb8a6a9809ddfa6aa8de1981d
Mike Fowler <mike.fowler@gov.bc.ca>
2025-10-07 14:20:06 -0700

    Messaging improvements
    Added time start, end and elapsed to messaging

--- Extract_AGOL_Layers_InputCSV.R
+++ Extract_AGOL_Layers_InputCSV.R
@@ -72,127 +72,168 @@ sendEmail <- function(to,subject,body,html=FALSE){
     OutApp$Quit()  
   }
 }
-#-------------------------------------------------------------------------------
-#--Postgres connection information 
-#-------------------------------------------------------------------------------
-pg_db = "prov_data"
-pg_host = "localhost"
-pg_port = 5432
-pg_user = "postgres"
-pg_password = "postgres"
-#--directory where this script is located
-#dir <- dirname(rstudioapi::getSourceEditorContext()$path)
-dir <- getCurrentFileLocation()
-csv_file <- glue(dir, '\\AGOL_Layers_to_Extract.csv')
-wd <- dir
-#-------------------------------------------------------------------------------
-#--Get a authorization token to our AGOL Portal 
-#-------------------------------------------------------------------------------
-token <- arcgisutils::auth_user(
-  username = Sys.getenv("ARCGIS_USER"),
-  password = Sys.getenv("ARCGIS_PASSWORD"),
-  host = "https://governmentofbc.maps.arcgis.com",
-  expiration = 60
-)
-arcgisutils::set_arc_token(token)
-#-------------------------------------------------------------------------------
-#--Now we loop through the input CSV file 
-input_csv <- read.csv(csv_file, header=TRUE)
-#--Create a dataframe to track what we update.  Will use this after to send an update email
-log_df <- data.frame(
-  WebLayer = c(), ServiceUrl = c(), Query=c(), OutPGSchema=c(),OutGPKG=c(),OutTable=c()
-)
-for (i in 1:nrow(input_csv)) {
-  #--Only process the active records
-  if (input_csv$ACTIVE[i]=='Y'){
-    #--Pull the values from the CSV into variables
-    web_layer <- input_csv$WEB_LAYER[i]
-    #layer_index <- input_csv$LAYER_INDEX[i]
-    service_url <- input_csv$SERVICE_URL[i]
-    query <- input_csv$QUERY[i]
-    out_schema <- input_csv$OUT_PG_SCHEMA[i]
-    out_gpkg <- input_csv$OUT_GPKG[i]
-    out_table <- input_csv$OUT_TABLE[i]
-    comments <- input_csv$COMMENTS[i]
-    #--Get down to business
+time_delta <- function(time1, time2){
+  time_diff = ""
+  if(as.numeric(difftime(time2, time1, units = "secs"))<60){
+    time_diff <- glue(difftime(time2, time1, units = "secs"), ' Seconds')
+  }else if(as.numeric(difftime(time2, time1, units = "mins"))<60){
+    time_diff <- glue(difftime(time2, time1, units = "mins"), ' Minutes')
+  }else{
+    time_diff <- glue(difftime(time2, time1, units = "hours"), ' Hours')
+  }
+  return(time_diff)
+}
+main <- function (){
+  tryCatch(
+    {
+    #--Suppress warnings
+    options(warn = -1)
+    #-------------------------------------------------------------------------------
+    #--Postgres connection information 
+    #-------------------------------------------------------------------------------
+    pg_db = "prov_data"
+    pg_host = "localhost"
+    pg_port = 5432
+    pg_user = "postgres"
+    pg_password = "postgres"
+    #--directory where this script is located
+    #dir <- dirname(rstudioapi::getSourceEditorContext()$path)
+    dir <- getCurrentFileLocation()
+    csv_file <- glue(dir, '\\AGOL_Layers_to_Extract.csv')
+    wd <- dir
+    #-------------------------------------------------------------------------------
+    #--Get a authorization token to our AGOL Portal 
+    #-------------------------------------------------------------------------------
+    token <- arcgisutils::auth_user(
+      username = Sys.getenv("ARCGIS_USER"),
+      password = Sys.getenv("ARCGIS_PASSWORD"),
+      host = "https://governmentofbc.maps.arcgis.com",
+      expiration = 60
+    )
+    arcgisutils::set_arc_token(token)
+    #-------------------------------------------------------------------------------
+    #--Now we loop through the input CSV file 
+    input_csv <- read.csv(csv_file, header=TRUE)
+    #--Create a dataframe to track what we update.  Will use this after to send an update email
+    log_df <- data.frame(
+      WebLayer=c(),ServiceUrl=c(),Query=c(),OutPGSchema=c(),OutGPKG=c(),OutTable=c(), 
+      TimeStart=c(),cTimeEnd=c(),TimeElapse=c()
+    )
+    for (i in 1:nrow(input_csv)) {
+      #--Only process the active records
+      if (input_csv$ACTIVE[i]=='Y'){
+        time_start <- as.POSIXct(now())
+        #--Pull the values from the CSV into variables
+        web_layer <- input_csv$WEB_LAYER[i]
+        #layer_index <- input_csv$LAYER_INDEX[i]
+        service_url <- input_csv$SERVICE_URL[i]
+        query <- input_csv$QUERY[i]
+        out_schema <- input_csv$OUT_PG_SCHEMA[i]
+        out_gpkg <- input_csv$OUT_GPKG[i]
+        out_table <- input_csv$OUT_TABLE[i]
+        comments <- input_csv$COMMENTS[i]
+        #--Get down to business
+        print('-----------------------------------------------------------')
+        print(glue('Processing {web_layer}'))
+        print(glue('Time Start: {time_start}'))
+        #--Connect to the AGOL Web Layer Service URL 
+        service_info <- arc_open(service_url)
+        layer <- service_info
+        #--Get the individual layer from the Web Layer (potentially a group layer)
+        #layer <- get_layer(service_info, 0)
+        #layer <- get_layer(service_info)
+        #--Get the data, apply the query if supplied
+        if(!is.na(query) & trimws(query) != ""){
+          data <- arc_select(layer, where = query)
+        }else{
+          data <- arc_select(layer)
+        }
+        #--Create and SF object from our data
+        sf_object <- st_as_sf(data, geometry=df$geometry, crs = service_info$spatialReference$latestWkid)
+        #plot(st_geometry(sf_object))
+        #--Drop problem fields (SE_ANNO_CAD_DATA must be here, it's a list field and throws an error)
+        drop_fields <- c("SE_ANNO_CAD_DATA","SHAPE__AREA", "SHAPE__LENGTH", "FEATURE_AREA", "FEATURE_PERIMETER", 
+                         "FEATURE_LENGTH_M")  #"FEATURE_AREA_SQM"
+        sf_object <- sf_object[,!(toupper(names(sf_object)) %in% drop_fields)]
+        #----------------------------------------------------------------------------------------
+        #--Extract the layer to Geopackage, if specified
+        #----------------------------------------------------------------------------------------
+        if(!is.na(out_gpkg)){
+          print(glue('----Extracting Web Layer to Geopackage {out_gpkg}:{out_table}'))
+          st_write(sf_object, file.path(out_gpkg), layer = out_table, driver = "GPKG", append = FALSE)  
+        }
+        #----------------------------------------------------------------------------------------
+        #--Extract the layer to Postgres, if specified
+        #----------------------------------------------------------------------------------------
+        if(!is.na(out_schema)){
+          print(glue('----Extracting Web Layer to Postgres {out_schema}.{out_table}'))
+          conn <- dbConnect(RPostgres::Postgres(),
+                            dbname = pg_db,
+                            host = pg_host,
+                            port = pg_port,
+                            user = pg_user,
+                            password = pg_password)
+          #--Write the layer out to Postgres
+          st_write(obj = sf_object,
+                   dsn = conn,
+                   Id(schema=out_schema, 
+                      table = tolower(out_table)),
+                   append = FALSE, # Set to TRUE to append data to an existing table
+                   delete_layer = TRUE)
+          #--Add a comment to the table
+          upd_time <- format(Sys.time(), "%A, %B %d, %Y %H:%M:%S %Z")
+          dbExecute(conn, glue("COMMENT ON TABLE {out_schema}.{out_table} IS '{comments} \nUpdate Time: {upd_time}';"))
+          
+          #-Disconnect from Postgres
+          dbDisconnect(conn)
+        }
+        time_end <- as.POSIXct(now())
+        time_elapse <- time_delta(time_start, time_end)
+        print('')
+        print('Processing Complete')
+        print(glue('Time Finished: {time_end}'))
+        print(glue('Total Elapsed: {time_elapse}'))
+        print('-----------------------------------------------------------')
+        #--Add a row to our logging dataframe
+        log_row <- data.frame(WebLayer = web_layer, ServiceUrl = service_url,Query=query,OutPGSchema=out_schema,
+                              OutGPKG=out_gpkg,OutTable=out_table,
+                              TimeStart=time_start,TimeEnd=time_end,TimeElapse=time_elapse)
+        log_df <- rbind(log_df, log_row)
+      }
+    }
+    #-------------------------------------------------------------------------------
+    #---Send an email here
+    #-------------------------------------------------------------------------------
+    #datestr <- format(Sys.time(), '%Y-%m-%d %H:%M:%OS')
+    #datestr <- format(Sys.time(), '%A, %B %d, %Y')
+    datestr <- format(Sys.time(), '%A, %B %d, %Y %H:%M:%S')
+    #html_output <- htmlTable(log_df, caption = "AGOL Extracted Data")
+    body <- ""
+    for (j in 1:nrow(log_df)) {
+      body <- paste0(body, 
+                     "------------------------------------------------------------",
+                     "\n")
+      row <- log_df[j,]
+      for (k in colnames(row)){
+        body <- paste(body, k,":",row[1,k], "\n")
+      }
+      body <- paste0(body, 
+                     "------------------------------------------------------------",
+                     "\n")
+    }
+    subj <- paste0("AGOL Extract: ", datestr)
+    email_sent <- sendEmail(to='mike.fowler@gov.bc.ca',
+               subject=subj,
+               body=body,html=FALSE)
     
-    #--Connect to the AGOL Web Layer Service URL 
-    service_info <- arc_open(service_url)
-    layer <- service_info
-    #--Get the individual layer from the Web Layer (potentially a group layer)
-    #layer <- get_layer(service_info, 0)
-    #layer <- get_layer(service_info)
-    #--Get the data, apply the query if supplied
-    if(!is.na(query) & trimws(query) != ""){
-      data <- arc_select(layer, where = query)
-    }else{
-      data <- arc_select(layer)
-    }
-    #--Create and SF object from our data
-    sf_object <- st_as_sf(data, geometry=df$geometry, crs = service_info$spatialReference$latestWkid)
-    #plot(st_geometry(sf_object))
-    #--Drop problem fields (SE_ANNO_CAD_DATA must be here, it's a list field and throws an error)
-    drop_fields <- c("SE_ANNO_CAD_DATA","SHAPE__AREA", "SHAPE__LENGTH", "FEATURE_AREA", "FEATURE_PERIMETER", 
-                     "FEATURE_AREA_SQM","FEATURE_LENGTH_M")
-    sf_object <- sf_object[,!(toupper(names(sf_object)) %in% drop_fields)]
-    #----------------------------------------------------------------------------------------
-    #--Extract the layer to Geopackage, if specified
-    #----------------------------------------------------------------------------------------
-    if(!is.na(out_gpkg)){
-      print(glue('----Extracting Web Layer to Geopackage {out_gpkg}:{out_table}'))
-      st_write(sf_object, file.path(out_gpkg), layer = out_table, driver = "GPKG", append = FALSE)  
-    }
-    #----------------------------------------------------------------------------------------
-    #--Extract the layer to Postgres, if specified
-    #----------------------------------------------------------------------------------------
-    if(!is.na(out_schema)){
-      print(glue('----Extracting Web Layer to Postgres {out_schema}.{out_table}'))
-      conn <- dbConnect(RPostgres::Postgres(),
-                        dbname = pg_db,
-                        host = pg_host,
-                        port = pg_port,
-                        user = pg_user,
-                        password = pg_password)
-      #--Write the layer out to Postgres
-      st_write(obj = sf_object,
-               dsn = conn,
-               Id(schema=out_schema, 
-                  table = tolower(out_table)),
-               append = FALSE, # Set to TRUE to append data to an existing table
-               delete_layer = TRUE)
-      #--Add a comment to the table
-      upd_time <- format(Sys.time(), "%A, %B %d, %Y %H:%M:%S %Z")
-      dbExecute(conn, glue("COMMENT ON TABLE {out_schema}.{out_table} IS '{comments} \nUpdate Time: {upd_time}';"))
-      
-      #-Disconnect from Postgres
-      dbDisconnect(conn)
-    }
-    log_row <- data.frame(WebLayer = web_layer, ServiceUrl = service_url,Query=query,OutPGSchema=out_schema,
-                          OutGPKG=out_gpkg,OutTable=out_table)
-    log_df <- rbind(log_df, log_row)
-  }
+    },
+    error = function(cond){#--Put warnings back on 
+      options(warn = 0)
+      message(conditionMessage(cond))
+    }, 
+    finally = {options(warn = 0)}
+  )
 }
-#-------------------------------------------------------------------------------
-#---Send an email here
-#-------------------------------------------------------------------------------
-#datestr <- format(Sys.time(), '%Y-%m-%d %H:%M:%OS')
-#datestr <- format(Sys.time(), '%A, %B %d, %Y')
-datestr <- format(Sys.time(), '%A, %B %d, %Y %H:%M:%S')
-#html_output <- htmlTable(log_df, caption = "AGOL Extracted Data")
-body <- ""
-for (j in 1:nrow(log_df)) {
-  body <- paste0(body, 
-                 "------------------------------------------------------------",
-                 "\n")
-  row <- log_df[j,]
-  for (k in colnames(row)){
-    body <- paste(body, k,":",row[1,k], "\n")
-  }
-  body <- paste0(body, 
-                 "------------------------------------------------------------",
-                 "\n")
-}
-subj <- paste0("AGOL Extract:", datestr)
-email_sent <- sendEmail(to='mike.fowler@gov.bc.ca',
-           subject=subj,
-           body=body,html=FALSE)
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+main()
