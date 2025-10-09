bd60d9e68680c57e4456722a85325db4195b772f
Mike Fowler <mike.fowler@gov.bc.ca>
2025-10-09 09:48:33 -0700

    Update to sendEmail & DESCRIPTION

--- R/sendEmail.R
+++ R/sendEmail.R
@@ -8,10 +8,16 @@
 #'
 #' @examples coming soon
 #' 
+
+#--This could be a stumbling block for other package users
+#--May need to be clear in the README.md that you need to download and have this installed from a specific 
+#--repo before installing the agolextract package 
+#--devtools::install_github("omegahat/RDCOMClient")
+library(RDCOMClient) 
 sendEmail <- function(to,subject,body){
   
   OutlookOpenAlready <- AGOLextract::isOutlookRunning()
-  OutApp <- RDCOMClient::COMCreate("Outlook.Application")
+  OutApp <- COMCreate("Outlook.Application")
   outMail <- OutApp$CreateItem(0) # 0 represents olMailItem
   outMail[["To"]] <- to
   outMail[["subject"]] <- subject
