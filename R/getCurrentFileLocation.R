9a0f8b2c0a4099cbdf90d9624a87577cd66accd5
Mike Fowler <mike.fowler@gov.bc.ca>
2025-10-09 09:20:59 -0700

    Update getCurrentFileLocation.R

--- R/getCurrentFileLocation.R
+++ R/getCurrentFileLocation.R
@@ -7,10 +7,10 @@
 #' 
 getCurrentFileLocation <-  function()
 {
-  this_file <- commandArgs() %>% 
-    tibble::enframe(name = NULL) %>%
-    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') %>%
-    dplyr::filter(key == "--file") %>%
+  this_file <- commandArgs() |> 
+    tibble::enframe(name = NULL) |>
+    tidyr::separate(col=value, into=c("key", "value"), sep="=", fill='right') |>
+    dplyr::filter(key == "--file") |>
     dplyr::pull(value)
   if (length(this_file)==0)
   {
